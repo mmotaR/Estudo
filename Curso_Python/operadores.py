887338e658c1143cad59294c8597c34a8651c317
Matheus Mota <matheusmota415@icloud.com>
2026-03-19 22:20:33 -0300

    Operadores Matemáticos

---
+++ Curso_Python/operadores.py
@@ -0,0 +1,20 @@
+# Operadores Matemáticos
+
+# Operações básicas: +, -, *, /, **, //, %
+# % Resto da divisão ! Ex. 10 % 3 = 1 ou 4 % 2 = 0
+# ** elevado ! Ex. 2 ** 3 = 8 ou 5 ** 2 = 25 (potenciação)
+
+# Operadores de comparação: ==, !=, >, <, >=, <=
+# == Igual a ! Ex. 5 == 5 é True ou 3 == 4 é False
+# != Diferente de ! Ex. 5 != 4 é True ou 3 != 3 é False
+# > Maior que ! Ex. 5 > 3 é True ou 2 > 4 é False
+# < Menor que ! Ex. 3 < 5 é True ou 4 < 2 é False
+# >= Maior ou igual a ! Ex. 5 >= 5 é True ou 4 >= 5 é False
+# <= Menor ou igual a ! Ex. 3 <= 4 é True ou 5 <= 4 é False 
+
+print(2 + 3)  # Soma
+print(5 - 2)  # Subtração
+print(4 * 3)  # Multiplicação   
+print(10 / 2)  # Divisão
+print(2 ** 3)  # Potenciação    
+print(10 % 3)  # Resto da divisão
